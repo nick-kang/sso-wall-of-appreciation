040b8225994db82e1233db62e6975c3e40b2e6e0
nick-kang <89998043+nick-kang@users.noreply.github.com>
2022-08-13 16:19:50 -0700

    update to handle api calls

--- functions/fn-format-request/src/index.ts
+++ functions/fn-format-request/src/index.ts
@@ -2,17 +2,22 @@ function handler(
   event: AWSCloudFrontFunction.Event,
 ): AWSCloudFrontFunction.Request {
   const request = event.request
+
+  if (request.method !== 'GET' && request.method !== 'OPTION') {
+    return request
+  }
+
   const uri = request.uri
 
   // Check if at root path
   if (uri === '/') {
     request.uri += 'index.html'
 
-    // Check whether the URI is missing a file name
+    // Check whether the URI ends with "/"
   } else if (uri.endsWith('/')) {
     request.uri = request.uri.slice(0, -1) + '.html'
 
-    // Check whether the URI is missing a file extension.
+    // Check whether the URI is missing a file extension
   } else if (!uri.includes('.')) {
     request.uri += '.html'
   }
