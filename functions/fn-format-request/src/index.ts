edd650cf0ecffbd12e8cc704931bd3b5c34e5cee
nick-kang <89998043+nick-kang@users.noreply.github.com>
2022-08-13 16:38:03 -0700

    improve comments

--- functions/fn-format-request/src/index.ts
+++ functions/fn-format-request/src/index.ts
@@ -4,16 +4,14 @@ function handler(
   const request = event.request
   const uri = request.uri
 
-  // Check if at root path
   if (uri === '/') {
+    // turns "/" to "/index.html"
     request.uri += 'index.html'
-
-    // Check whether the URI ends with "/"
   } else if (uri.endsWith('/')) {
-    request.uri = request.uri.slice(0, -1) + '.html'
-
-    // Check whether the URI is missing a file extension
+    // turns "/blog/" to "/blog.html"
+    request.uri = uri.slice(0, -1) + '.html'
   } else if (!uri.includes('.')) {
+    // turns "/blog" to "/blog.html"
     request.uri += '.html'
   }
 
